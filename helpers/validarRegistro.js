222505e14c5b881e8fe84291d2bd403c8c248410
Jcmedinah <jean.medina0399@gmail.com>
2025-09-13 14:54:09 -0500

    (Develop): Creacion de helper validarRegistro

---
+++ helpers/validarRegistro.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+let inputs = document.querySelectorAll('.formulario__entrada')
+
+
+let arregloInputs = [...inputs]
+console.log(inputs)
+console.log(arregloInputs)
+
+
+arregloInputs.map((input)=>{
+    console.log(input)
+})
